2e96f4c48f83340a963e1df8b87a04d5d386f4cc
Ghiles-H <hallou.ghiles@gmail.com>
2022-01-27 15:02:57 +0100

    Start_New_Version_27-01-2022

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 // Import
-import  NotFound  from "./pages/NotFound";
+
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-//import ResponsiveAppBar from "./components/Banner";
-//import RecipeReviewCard from "./components/Posts";
-import Home from "./pages/HomePage";
-import Profil from "./pages/ProfilPage";
+
+
 import "./styles/App.css";
-import  Login  from "./pages/Login";
-import Sign_Up from "./pages/Sign_Up";
+
 // Components
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <Route path="/home" exact component={ Home } />
-          <Route path="/profil" exact component={Profil} />
-          <Route path="/login" exact component={ Login } />
-          <Route path="/signup" exact component = {Sign_Up} />
-          
-          <Route  component={NotFound} />
-          </Switch>
+        <Switch></Switch>
       </BrowserRouter>
     </div>
   );
